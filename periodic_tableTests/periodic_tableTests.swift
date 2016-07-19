065ad3e812b1875e75ffffce41c27001c96d74cf
Scott Courtney <scott.courtney@qonceptual.com>
2016-07-18 20:55:59 -0400

    Initial Commit

---
+++ periodic_tableTests/periodic_tableTests.swift
@@ -0,0 +1,36 @@
+//
+//  periodic_tableTests.swift
+//  periodic_tableTests
+//
+//  Created by Scott Courtney on 7/18/16.
+//  Copyright © 2016 Scott Courtney. All rights reserved.
+//
+
+import XCTest
+@testable import periodic_table
+
+class periodic_tableTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
